388da8680d5f1f2b99d48f8e7e208015f0f47346
Thomas Johander <lopan@Thomas-Johanders-MacBook-Pro.local>
2017-02-15 16:52:22 -0600

    Location data writing to the DOM

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+// ["IL", 5000, 12]
+// ["MN", 300, 3]
+// ["NV", 25000, 1]
+
+
 var locations = []; // this array will hold your objects
 
 // object constructor
 
+function Location(state,revenue,locationCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function(){
+    return this.revenue / this.locationCount;
+  }
+}
+
+
 // object instances
 
+var il = new Location('IL',5000,12);
+var mn = new Location('MN',300,3);
+var nv = new Location('NV',25000,1);
+
+// new Location('NV',25000,1);
+
 // push object instances to locations array
+locations.push(il,mn,nv)
+console.log(locations);
+
+
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
